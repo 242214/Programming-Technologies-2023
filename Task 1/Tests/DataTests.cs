2d093c83768516d88277d5f4cbcb72f2bfca3423
242214 <236532@edu.p.lodz.pl>
2023-04-01 19:40:30 +0200

    DataTest Fixed

--- Task 1/Tests/DataTests.cs
+++ Task 1/Tests/DataTests.cs
@@ -1,11 +1,70 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Data;
+using Data.API;
+using Data.Implementation;
+using Logic.Implementation;
+
 namespace Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class CheckAddingObjects
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CheckAddinggCustomer()
         {
+            IDatabase database = new Database();
+            database.AddCustomer(1, "John", "John");
+            Assert.AreEqual(database.CountCustomerList(), 1);
+            try
+            {
+                database.AddCustomer(1, "Kate", "Jonie");
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine("Trying to add a customer with the same id.");
+            }
+            Assert.AreEqual(database.CountCustomerList(), 1);
+            database.DeleteCustomer(1);
+            Assert.AreEqual(database.CountCustomerList(), 0);
+        }
+
+        [TestMethod]
+        public void CheckAddingDeletingOrder()
+        {
+            IDatabase database = new Database();
+            database.AddOrder(1, 1, 2);
+            Assert.AreEqual(database.CountOrderList(), 1);
+            try
+            {
+                database.AddOrder(1, 4, 2);
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine("Trying to add an ordern order with the same id.");
+            }
+            Assert.AreEqual(database.CountOrderList(), 1);
+            database.DeleteOrder(1);
+            Assert.AreEqual(database.CountOrderList(), 0);
+        }
+
+        [TestMethod]
+        public void CheckAddingDeletingProduct()
+        {
+            IDatabase database = new Database();
+            database.AddProduct(1, "Red Apple", 0.5, 100);
+            Assert.AreEqual(database.CountProductList(), 1);
+            try
+            {
+                database.AddProduct(1, "Bread", 2.5, 500);
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine("Trying to add product with the same id.");
+            }
+            Assert.AreEqual(database.CountProductList(), 1);
+            database.DeleteProduct(1);
+            Assert.AreEqual(database.CountProductList(), 0);
         }
     }
 }
