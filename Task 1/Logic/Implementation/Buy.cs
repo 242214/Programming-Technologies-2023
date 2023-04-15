2d652cbc3847528c379ce88765698ba632b60890
242214 <236532@edu.p.lodz.pl>
2023-04-15 14:57:13 +0200

    k

--- Task 1/Logic/Implementation/Buy.cs
+++ Task 1/Logic/Implementation/Buy.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 using System;
 using Data.API;
 using Data.Implementation;
@@ -17,4 +18,23 @@ internal class Buy : IBuy {
         this.Amount = Amount;
         this.UserId = 0;
     }
+=======
+using System;
+using Data.API;
+using Data.Implementation;
+using Logic.API;
+namespace Logic.Implementation;
+
+internal class Buy {
+    public int Id { get; set; }
+    public int ProductId { get; set; }
+    public uint Amount { get; set; }
+    public int UserId { get    public Buy(int Id, int ProductId, uint Amount)
+    {
+        this.Id = Id;
+        this.ProductId = ProductId;
+        this.Amount = Amount;
+        this.UserId = 0;
+    }
+9b24328fae5c9423c2
 }
