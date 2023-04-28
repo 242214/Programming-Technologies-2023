117797c60448cd3629ae2294936d0fc0a53cfcc3
242218 Małgorzata Komorowska <242218@edu.p.lodz.pl>
2023-04-28 17:34:29 +0200

    IDataContext addaed

--- Task 2/Data/API/IDataContext.cs
+++ Task 2/Data/API/IDataContext.cs
@@ -1,6 +1,22 @@
-﻿namespace Data.API
+﻿using Data.Implementation;
+
+namespace Data.API
 {
-    internal class IDataContext
+    public interface IDataContext
     {
+    public IQueryable<ICustomer> Customer { get;}
+    public IQueryable<IProduct> Product { get;}
+    public IQueryable<IOrder> Order { get; }
+    public IQueryable<IState> State { get; }
+
+    public static IDataContext CreateContext(string? connectionString = null) => new DataContext(connectionString);
+    Task AddCustomerAsync(ICustomer customer);
+    Task AddStateAsync(IState state);
+    Task AddProductAsync(IProduct product);
+    Task AddOrderAsync(IOrder order);
+    Task DeleteCustomerAsync(int Id);
+    Task DeleteStateAsync(int Id);
+    Task DeleteProductAsync(int Id);
+    Task DeleteOrderAsync(int Id);
     }
 }
