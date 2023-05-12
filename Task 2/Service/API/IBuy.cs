73d9735d8968f94dcb4782c5daa22ba39567f330
242218 Małgorzata Komorowska <242218@edu.p.lodz.pl>
2023-05-12 16:32:41 +0200

    IBuy added in services/API

---
+++ Task 2/Service/API/IBuy.cs
@@ -0,0 +1,10 @@
+﻿namespace Service.API;
+public interface IBuy : IOrder
+{
+    public int Id { get; set; }
+    public int ProductId { get; set; }
+    public uint Amount { get; set; }
+    public int UserId { get; set; }
+    
+    Task AddAsync();
+}
