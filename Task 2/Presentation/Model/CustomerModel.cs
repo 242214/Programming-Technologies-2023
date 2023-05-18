c93a1ae49a52243fea18ced0628d89333b2bc3f9
242214 <236532@edu.p.lodz.pl>
2023-05-18 13:11:09 +0200

    ViewModel

--- Task 2/Presentation/Model/CustomerModel.cs
+++ Task 2/Presentation/Model/CustomerModel.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Service.API;
+using Data.API;
 namespace Presentation.Model
 {
-public class CustomerModel : Service.API.ICustomer
+public class CustomerModel : ICustomer
 {
     public int Id { get; set; }
     public string FirstName { get; set; }
---
+++ Task 2/Presentation/ViewModel/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Data.API;
+
+namespace Presentation.ViewModel
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        public MainViewModel() 
+        {
+            //Data = IDataRepository.CreateDatabase();
+            CustomerViewModel = new CustomerViewModel(new CustomerModel());
+        }
+    }
+}
