5bc49c1e409bb2daac7e8127fd934d8dafac52ee
242218 Małgorzata Komorowska <242218@edu.p.lodz.pl>
2023-05-18 11:32:02 +0200

    Customer and Order in ViewModel

---
+++ Task 2/Presentation/ViewModel/CustomerViewModel.cs
@@ -0,0 +1,52 @@
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Presentation.Model;
+using Data.API;
+
+namespace Presentation.ViewModel 
+{
+    public partial class CustomerViewModel: ObservableObject
+    {
+        private ICustomer _customer;
+        public CustomerViewModel()
+        {
+        }
+        public CustomerViewModel(ICustomer customer)
+        {
+            _customer = customer;
+        }
+        public int CustomerId {
+            get =>  _customer.Id;
+            set
+            {
+                _customer.Id = value;
+                OnPropertyChanged();
+            } }
+        public string Name { 
+            get => _customer.FirstName;
+            set
+            {
+                _customer.FirstName = value;
+                OnPropertyChanged();
+            } }
+        public string Surname { 
+            get => _customer.LastName;
+            set
+            {
+                _customer.LastName = value;
+                OnPropertyChanged();
+            } }
+        // //[ICommand]
+        // private async Task AddCustomer()
+        // {
+        //     await _customer.AddAsync();
+        // }
+        // //[ICommand]
+        // private async Task DeleteCustomer()
+        // {
+        //     await _customer.DeleteAsync();
+        // }
+
+    }
+}
