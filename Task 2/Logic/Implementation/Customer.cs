60d8c5230a1cc37ced3edbdd3fb1decfaae8df8b
242218 Małgorzata Komorowska <242218@edu.p.lodz.pl>
2023-05-20 11:51:42 +0200

    classes added in Logic Implementation

---
+++ Task 2/Logic/Implementation/Customer.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data;
+using Data.API;
+
+namespace Service.Implementation
+{
+    internal class Customer : ICustomer
+    {
+        public Customer(int Id, string FirstName, string LastName)
+        {
+        this.Id = Id;
+        this.FirstName = FirstName;
+        this.LastName = LastName;
+        }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
